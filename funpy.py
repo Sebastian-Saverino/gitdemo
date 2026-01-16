4403675212eef2c6d23bd792c361847050efc6e9
Sebastian Saverino <sebastian.saverino4@gmail.com>
2026-01-15 17:50:51 -0800

    import pandas as pd

--- funpy.py
+++ funpy.py
@@ -1,11 +1,15 @@
+import pandas as pd
 x = int(input("Enter a number: "))
 
 print("You entered:", x)
 print(x)
 print("Hello, FunPy!")
+
+
 y = []
 for i in range(x):
     y.append(i)
+
 
 print(y)
 print("List of numbers from 0 to", x-1, ":", y)
